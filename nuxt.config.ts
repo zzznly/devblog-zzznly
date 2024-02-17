619be3396d08bc2ba53120e7f4330d180e0e5a57
jinri yoo <zzznly@gmail.com>
2024-02-18 03:07:40 +0900

    에러로 인한 패키지 변경

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@hebilicious/vue-query-nuxt',
-    '@pinia/nuxt',
-  ],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   postcss: {
     plugins: {
       tailwindcss: {},
