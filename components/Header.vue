3778589c18a3abe7b1c4df6f65c6a27797cef539
jinri yoo <zzznly@gmail.com>
2024-02-17 12:16:44 +0900

    header 수정

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <Disclosure v-slot="{ open }" as="nav" class="bg-white">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
