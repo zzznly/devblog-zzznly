0b09bc7b0eb286f89a48d0a55f811713d94424ee
jinri yoo <zzznly@gmail.com>
2024-02-18 00:31:17 +0900

    로그인 페이지 생성

---
+++ pages/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mx-auto py-10 text-center">
+    <h2 class="text-3xl">Login Page</h2>
+    <p class="mt-2 text-sm">로그인 페이지입니다</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
